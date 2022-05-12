d1601927b07682a20b641ac6393971617d11ecbf
Shakiba448 <shakiba448@gmail.com>
2022-05-12 22:47:06 +0600

    WIP on function

--- src/components/Xstate.tsx
+++ src/components/Xstate.tsx
@@ -7,10 +7,28 @@ const Xstate = () => {
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="flex flex-col bg-gray-600 p-3 rounded-2xl space-y-2">
-        <div className="h-12 w-12 rounded-full bg-red-500 cursor-pointer" />
-        <div className="h-12 w-12 rounded-full bg-yellow-500 cursor-pointer" />
-        <div className="h-12 w-12 rounded-full bg-green-500 cursor-pointer" />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2  rounded">
+        <div
+          className={`h-12 w-12 rounded-full cursor-pointer ${
+            state.matches("red") ? "bg-red-500 " : "bg-red-900 "
+          }`}
+          onClick={() => state.matches("red")}
+        />
+        <div
+          className={`h-12 w-12 rounded-full cursor-pointer ${
+            state.matches("yellow") ? "bg-yellow-500 " : "bg-yellow-900 "
+          }`}
+          onClick={() => state.matches("yellow")}
+        />
+        <div
+          className={`h-12 w-12 rounded-full cursor-pointer ${
+            state.matches("green") ? "bg-green-500 " : "bg-green-900 "
+          }`}
+          onClick={() => state.matches("green")}
+        />
+        <button
+          onClick={() => send("NEXT")}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2  rounded"
+        >
           Next
         </button>
       </div>
