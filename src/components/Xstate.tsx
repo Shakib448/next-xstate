f765f42a23c870e8ca495589d8f51bcbf60d8db3
Shakiba448 <shakiba448@gmail.com>
2022-05-12 22:28:52 +0600

    WIP on function

--- src/components/Xstate.tsx
+++ src/components/Xstate.tsx
@@ -10,6 +10,9 @@ const Xstate = () => {
         <div className="h-12 w-12 rounded-full bg-red-500 cursor-pointer" />
         <div className="h-12 w-12 rounded-full bg-yellow-500 cursor-pointer" />
         <div className="h-12 w-12 rounded-full bg-green-500 cursor-pointer" />
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2  rounded">
+          Next
+        </button>
       </div>
     </div>
   );
