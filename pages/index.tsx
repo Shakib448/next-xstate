6a6b509392ba30d10e8bcd6d60eca6cd4bf24efa
Shakiba448 <shakiba448@gmail.com>
2022-05-14 11:38:42 +0600

    Done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
 import Layout from "@common/Layout";
 import JokeSearch from "@components/JokeSearch";
-// import Xstate from "@components/Xstate";
-
 import { ReactElement } from "react";
 
 const Home = () => {
-  // return <Xstate />;
   return <JokeSearch />;
 };
 
